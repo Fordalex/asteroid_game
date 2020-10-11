e7368a319009cdf80fd0596a5582e26230548650
Alex <alex96ford19@gmail.com>
2020-10-11 17:18:53 +0100

    Added the logic for the bullet to be projected in the direction that the mouse is pointed.

--- javascript.js
+++ javascript.js
@@ -1,27 +1,43 @@
+// canvas setup
+
 var canvasWidth = 700;
 var canvasHeight = 700;
 
 function setup() {
     createCanvas(canvasWidth, canvasHeight)
-    for (let i = 0; i < 100; i++) {
+    noCursor();
+    for (let i = 0; i < 5; i++) {
         var newEnemy = new enemy();
         enemies.push(newEnemy)
     }
 }
 
+// draw loop
+
 function draw() {
     background(150);
     showPlayersStats();
     playerOne.draw()
+        // check if an object is colliding with the player.
     for (e of enemies) {
         e.draw()
-        if (e.intersects()) {
+        if (playerOne.intersects(e)) {
             playerOne.hit()
         }
     }
+    // draw all the bullets when shot.
+    for (b of bullets) {
+        b.draw()
+    }
+    // cursor
+    fill('rgba(0,0,0,0.7)');
+    circle(mouseX, mouseY, 10)
 }
 
+//  game logic
+
 var enemies = [];
+var bullets = [];
 
 class player {
     constructor() {
@@ -29,13 +45,13 @@ class player {
             'x': canvasWidth / 2,
             'y': canvasHeight / 2,
         }
-        this.diameters = 50;
+        this.diameter = 50;
         this.colour = 'rgba(255,255,255, 0.9)';
         this.life = 100;
     }
     draw() {
         fill(this.colour)
-        circle(this.position.x, this.position.y, this.diameters)
+        circle(this.position.x, this.position.y, this.diameter)
     }
     hit() {
         if (this.life > 0) {
@@ -44,7 +60,40 @@ class player {
         } else {
             endGame()
         }
+    }
+    intersects(e) {
+        var distance = dist(e.position.x, e.position.y, this.position.x, this.position.y)
+        var diameters = e.diameter / 2 + this.diameter / 2
+        return (distance < diameters)
+    }
+}
 
+var playerOne = new player();
+
+class bullet {
+    constructor() {
+        this.speed = 7;
+        this.position = {
+            x: playerOne.position.x,
+            y: playerOne.position.y,
+        };
+        this.velocity = {
+            x: 0,
+            y: 0,
+        }
+        this.diameter = 5;
+        this.colour = 'rgba(0,0,0,0.8)';
+    }
+    draw() {
+        fill(this.colour)
+        circle(this.position.x, this.position.y, this.diameter)
+        this.update()
+    }
+    update() {
+        for (let i = 0; i < this.speed; i++) {
+            this.position.x += this.velocity.x;
+            this.position.y += this.velocity.y;
+        }
     }
 }
 
@@ -52,15 +101,15 @@ class enemy {
     constructor() {
         this.position = {
             'x': Math.floor(Math.random() * canvasWidth),
-            'y': Math.floor(Math.random() * canvasHeight),
+            'y': 0,
         }
-        this.diameters = Math.floor(Math.random() * 50 + 10);
+        this.diameter = Math.floor(Math.random() * 50 + 10);
         this.colour = 'rgba(255,0,0, 0.5)';
-        this.speed = 0.1;
+        this.speed = 0.5;
     }
     draw() {
         fill(this.colour)
-        circle(this.position.x, this.position.y, this.diameters)
+        circle(this.position.x, this.position.y, this.diameter)
         this.movePosition()
     }
     movePosition() {
@@ -77,12 +126,10 @@ class enemy {
     }
     intersects() {
         var distance = dist(playerOne.position.x, playerOne.position.y, this.position.x, this.position.y)
-        var diameters = playerOne.diameters / 2 + this.diameters / 2
+        var diameters = playerOne.diameter / 2 + this.diameter / 2
         return (distance < diameters)
     }
 }
-
-var playerOne = new player();
 
 
 function showPlayersStats() {
@@ -93,4 +140,15 @@ function showPlayersStats() {
 
 function endGame() {
     $('#endGameModal').modal('show');
+}
+
+function mouseClicked() {
+    var newBullet = new bullet();
+    const angle = Math.atan2(
+        mouseX - playerOne.position.x,
+        mouseY - playerOne.position.y,
+    )
+    newBullet.velocity.x = Math.sin(angle)
+    newBullet.velocity.y = Math.cos(angle)
+    bullets.push(newBullet)
 }
