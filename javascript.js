82e4113945519da54b7b57b92604a47b66794ba3
Alex <alex96ford19@gmail.com>
2020-10-12 19:47:40 +0100

    Added a pause feature and added the logic for the medipack. Also, I rearanged the stats layout.

--- javascript.js
+++ javascript.js
@@ -24,6 +24,7 @@ var moneyMultiplierCost = 10;
 
 // canvas setup
 
+var gamePaused = false;
 var canvasWidth = 700;
 var canvasHeight = 700;
 
@@ -36,71 +37,78 @@ function setup() {
 
 function draw() {
     // check players score and increase the difficulty
-
     background(15);
     showPlayersStats();
-    playerOne.draw()
-        // check if an object is colliding with the player.
-    for (e of enemies) {
-        const angle = Math.atan2(
-            playerOne.position.x - e.position.x,
-            playerOne.position.y - e.position.y,
-        )
-        e.velocity.x = Math.sin(angle) / enemySpeed;
-        e.velocity.y = Math.cos(angle) / enemySpeed;
-        e.draw()
-        if (playerOne.intersects(e)) {
-            playerOne.hit()
-        }
-    }
-    // draw all the bullets when shot.
-    for (var i = 0; i < bullets.length; i++) {
-        var theBullet = bullets[i]
-        theBullet.draw()
-            // check if a bullet is hitting an enemy
-        for (let j = 0; j < enemies.length; j++) {
-            if (enemies[j].checkIfHit(theBullet)) {
-
-                if (enemies[j].dead()) {
-                    money += 5 * moneyMultiplier;
-                    score += 1;
-                    var velocity = 0;
-                    for (let i = 0; i < 15; i++) {
-                        var newDebri = new debri();
-                        newDebri.position.y = enemies[j].position.y;
-                        newDebri.position.x = enemies[j].position.x;
-                        newDebri.velocity.x = theBullet.velocity.x * 2 + Math.random() * 2;
-                        newDebri.velocity.y = theBullet.velocity.y * 2 + Math.random() * 2;
-                        newDebri.colour = enemies[j].colour;
-                        debris.push(newDebri)
-                        velocity += 4
-                    }
-                    enemies.splice(j, 1)
-                }
-                bullets.splice(i, 1)
-                break
+    if (!gamePaused) {
+        playerOne.draw()
+            // check if an object is colliding with the player.
+        for (e of enemies) {
+            const angle = Math.atan2(
+                playerOne.position.x - e.position.x,
+                playerOne.position.y - e.position.y,
+            )
+            e.velocity.x = Math.sin(angle) / enemySpeed;
+            e.velocity.y = Math.cos(angle) / enemySpeed;
+            e.draw()
+            if (playerOne.intersects(e)) {
+                playerOne.hit()
             }
         }
+        // draw all the bullets when shot.
+        for (var i = 0; i < bullets.length; i++) {
+            var theBullet = bullets[i]
+            theBullet.draw()
+                // check if a bullet is hitting an enemy
+            for (let j = 0; j < enemies.length; j++) {
+                if (enemies[j].checkIfHit(theBullet)) {
+
+                    if (enemies[j].dead()) {
+                        money += 5 * moneyMultiplier;
+                        score += 1;
+                        var velocity = 0;
+                        for (let i = 0; i < 15; i++) {
+                            var newDebri = new debri();
+                            newDebri.position.y = enemies[j].position.y;
+                            newDebri.position.x = enemies[j].position.x;
+                            newDebri.velocity.x = theBullet.velocity.x * 2 + Math.random() * 2;
+                            newDebri.velocity.y = theBullet.velocity.y * 2 + Math.random() * 2;
+                            newDebri.colour = enemies[j].colour;
+                            debris.push(newDebri)
+                            velocity += 4
+                        }
+                        enemies.splice(j, 1)
+                    }
+                    bullets.splice(i, 1)
+                    break
+                }
+            }
+        }
+        // draw all the debri
+        for (d of debris) {
+            d.draw()
+        }
+        // cursor
+        fill('rgba(255,255,255,0.9)');
+        circle(mouseX, mouseY, 5)
+        if (bullets.length > 50) {
+            bullets.shift()
+        }
+        if (debris.length > 50) {
+            debris.shift()
+        }
+        // stop the player if they have ran out of fule while moving
+        if (fule == 0) {
+            playerOne.velocity.x = 0;
+            playerOne.velocity.y = 0;
+        }
+        if (miniGunTimer > 0) {
+            miniGunTimer--;
+        }
+    } else {
+        textSize(30);
+        fill(200, 200, 200);
+        text('PAUSED', 290, 350);
     }
-    // draw all the debri
-    for (d of debris) {
-        d.draw()
-    }
-    // cursor
-    fill('rgba(255,255,255,0.9)');
-    circle(mouseX, mouseY, 5)
-    if (bullets.length > 50) {
-        bullets.shift()
-    }
-    if (debris.length > 50) {
-        debris.shift()
-    }
-    // stop the player if they have ran out of fule while moving
-    if (fule == 0) {
-        playerOne.velocity.x = 0;
-        playerOne.velocity.y = 0;
-    }
-    miniGunTimer++;
 }
 
 //  game logic
@@ -114,7 +122,7 @@ var bulletSpeed = 1;
 var bulletSize = 5;
 var fule = 50;
 var moneyMultiplier = 1;
-var miniGunTimer = 1500;
+var miniGunTimer = 0;
 var miniGunActivated = false;
 
 class player {
@@ -185,6 +193,10 @@ window.addEventListener('keydown', function(e) {
             playerOne.moveLeft()
         }
         fule--
+    } else {
+        textSize(15);
+        fill(200, 200, 200);
+        text('No fule!', 350, 350);
     }
 })
 
@@ -289,20 +301,21 @@ function showPlayersStats() {
     textSize(15);
     fill(200, 200, 200);
     text(`Life: ${playerOne.life}`, 10, 20);
-    text(`Score: ${score}`, 90, 20);
-    text(`Fule: ${fule}`, 200, 20);
-    text(`Money Multiplyer: ${moneyMultiplier}`, 300, 20);
-    text(`Money: ${money}`, 580, 20);
+    text(`Score: ${score}`, 10, 40);
+    text(`Money Multiplyer: ${moneyMultiplier}`, 10, 60);
+    text(`Money: ${money}`, 10, 80);
 
-    textSize(15);
-    text(`Enemy Delay: ${enemySpawnSpeed}`, 10, 690);
-    text(`Enemy Speed: ${enemySpeed.toFixed(2)}`, 150, 690);
-    text(`Bullet Speed: ${bulletSpeed}`, 290, 690);
-    text(`Bullet Size: ${bulletSize}`, 420, 690);
-    textSize(10);
-    text('Velocity', 640, 660);
-    text(`Y:${playerOne.velocity.y}`, 640, 675);
-    text(`X:${playerOne.velocity.x}`, 640, 690);
+    text(`Bullet Speed: ${bulletSpeed}`, 580, 20);
+    text(`Bullet Size: ${bulletSize}`, 580, 40);
+    text(`MiniGun: ${miniGunTimer}`, 580, 60);
+
+    text(`Enemy Delay: ${enemySpawnSpeed}`, 10, 670);
+    text(`Enemy Speed: ${enemySpeed.toFixed(2)}`, 10, 690);
+
+    text(`Fule: ${fule}`, 620, 630);
+    text('Velocity', 620, 650);
+    text(`Y:${playerOne.velocity.y.toFixed(2)}`, 620, 670);
+    text(`X:${playerOne.velocity.x.toFixed(2)}`, 620, 690);
     // text(`Time:`, 390, 30);
 }
 
@@ -313,7 +326,7 @@ function endGame() {
 
 
 function mouseMoved() {
-    if (miniGunActivated && miniGunTimer < 1500) {
+    if (miniGunActivated && miniGunTimer > 0) {
         var newBullet = new bullet();
         const angle = Math.atan2(
             mouseX - playerOne.position.x,
@@ -348,43 +361,45 @@ var enemySpeed = 2;
 var gameDifficulty = 1;
 
 function spawnEnemies() {
-    if (enemySpawnSpeed > 200) {
-        enemySpawnSpeed -= gameDifficulty;
-        if (enemySpeed > 0.15) {
-            enemySpeed -= gameDifficulty / 100;
-        } else {
-            enemySpeed = 0.15;
+    if (!gamePaused) {
+        if (enemySpawnSpeed > 200) {
+            enemySpawnSpeed -= gameDifficulty;
+            if (enemySpeed > 0.15) {
+                enemySpeed -= gameDifficulty / 1000;
+            } else {
+                enemySpeed = 0.15;
+            }
+
         }
 
-    }
-
-    for (let i = 0; i < enemySpawnAmount; i++) {
-        var newEnemy = new enemy();
-        var spawn = Math.floor(Math.random() * 5)
-        if (spawn > 3) {
-            newEnemy.position.x = Math.floor(Math.random() * canvasWidth)
-            newEnemy.position.y = 0
-        } else if (spawn > 2) {
-            newEnemy.position.x = 0
-            newEnemy.position.y = Math.floor(Math.random() * canvasHeight)
-        } else if (spawn > 1) {
-            newEnemy.position.x = Math.floor(Math.random() * canvasWidth)
-            newEnemy.position.y = canvasHeight
-        } else {
-            newEnemy.position.x = canvasWidth
-            newEnemy.position.y = Math.floor(Math.random() * canvasHeight)
+        for (let i = 0; i < enemySpawnAmount; i++) {
+            var newEnemy = new enemy();
+            var spawn = Math.floor(Math.random() * 5)
+            if (spawn > 3) {
+                newEnemy.position.x = Math.floor(Math.random() * canvasWidth)
+                newEnemy.position.y = 0
+            } else if (spawn > 2) {
+                newEnemy.position.x = 0
+                newEnemy.position.y = Math.floor(Math.random() * canvasHeight)
+            } else if (spawn > 1) {
+                newEnemy.position.x = Math.floor(Math.random() * canvasWidth)
+                newEnemy.position.y = canvasHeight
+            } else {
+                newEnemy.position.x = canvasWidth
+                newEnemy.position.y = Math.floor(Math.random() * canvasHeight)
+            }
+            const angle = Math.atan2(
+                playerOne.position.x - newEnemy.position.x,
+                playerOne.position.y - newEnemy.position.y,
+            )
+            newEnemy.velocity.x = Math.sin(angle) / enemySpeed;
+            newEnemy.velocity.y = Math.cos(angle) / enemySpeed;
+            enemies.push(newEnemy)
         }
-        const angle = Math.atan2(
-            playerOne.position.x - newEnemy.position.x,
-            playerOne.position.y - newEnemy.position.y,
-        )
-        newEnemy.velocity.x = Math.sin(angle) / enemySpeed;
-        newEnemy.velocity.y = Math.cos(angle) / enemySpeed;
-        enemies.push(newEnemy)
+        setTimeout(function() {
+            spawnEnemies();
+        }, enemySpawnSpeed)
     }
-    setTimeout(function() {
-        spawnEnemies();
-    }, enemySpawnSpeed)
 }
 spawnEnemies()
 
@@ -398,7 +413,11 @@ window.addEventListener('keypress', function(e) {
         }
     }
     if (e.key == 2) {
-
+        if (money >= medipackCost) {
+            money -= medipackCost;
+            playerOne.life += 50;
+            medipackCost += medipackCost;
+        }
     }
     if (e.key == 3) {
         if (money >= bulletSizeCost) {
@@ -419,7 +438,7 @@ window.addEventListener('keypress', function(e) {
             miniGunActivated = true;
             money -= miniGunCost;
             miniGunCost += miniGunCost;
-            miniGunTimer = 0;
+            miniGunTimer += 1500;
         }
     }
     if (e.key == 6) {
@@ -428,6 +447,14 @@ window.addEventListener('keypress', function(e) {
             moneyMultiplierCost += moneyMultiplierCost;
             moneyMultiplier += 1;
 
+        }
+    }
+    if (e.key == ' ') {
+        if (gamePaused) {
+            gamePaused = false;
+            spawnEnemies();
+        } else {
+            gamePaused = true;
         }
     }
     updateShop();
