ad7851dc5fce5ee25778dae1e90fc3127f5f819f
alex <alex96ford19@gmail.com>
2020-10-20 21:33:38 +0100

    Added socketio and d started the logic on pong. Also, added the main background image.

--- pong.py
+++ pong.py
@@ -4,9 +4,12 @@ if path.exists("env.py"):
     import env
 from flask import Flask, render_template, redirect, url_for, request
 from flask_pymongo import PyMongo, pymongo
+from flask_socketio import SocketIO, join_room
 from app import app
 
+
 mongo = PyMongo(app)
+socketio = SocketIO(app)
 
 @app.route('/pong_lobby')
 def pong_lobby():
@@ -21,4 +24,21 @@ def pong():
     Joins the player to pong game with the same game id.
     """
     playersName = request.args.get('playersName')
-    return render_template('pong/pong.html', playersName=playersName)
+    room = request.args.get('room')
+    return render_template('pong/pong.html', playersName=playersName, room=room)
+
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    """
+    Using sockets the room id will be passed into join_room to create a new room.
+    """
+    join_room(data['room'])
+    socketio.emit('join_room_announcement', data)
+
+
+@socketio.on('playerOne_position')
+def handle_playerOne_position(data):
+    """
+    Update player one's position.
+    """
+    socketio.emit('playerOne_position_received', data)
--- static/js/pong/main.js
+++ static/js/pong/main.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     $('#canvasContainer').html(canvas)
 });
 
+const socket = io.connect("http://localhost:5001/");
+
 var canvasHeight = 600;
 var canvasWidth = 600;
 
@@ -15,22 +17,34 @@ function setup() {
 var playerOneX = 20;
 var playerOneY = 20;
 
+var playerTwoX = canvasWidth - 20;
+var playerTwoY = 20;
+
 function draw() {
     background(15);
     rect(playerOneX, playerOneY, 10, 70);
-    rect(canvasWidth - 20, 10, 10, 70);
+    rect(playerTwoX, playerTwoY, 10, 70);
 }
 
 window.addEventListener('keydown', function(e) {
     if (e.key == 's') {
         if (playerOneY + 77 < canvasHeight) {
             playerOneY += 20;
+            socket.emit('playerOne_position', {
+                y: playerOneY,
+            })
         }
     }
     if (e.key == 'w') {
         if (playerOneY > 0) {
             playerOneY -= 20;
+            socket.emit('playerOne_position', {
+                y: playerOneY,
+            })
         }
     }
-   
+})
+
+socket.on('playerOne_position_received', function(e) {
+    playerOneY = e['y']
 })
